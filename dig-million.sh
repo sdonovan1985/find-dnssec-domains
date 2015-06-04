34cbe5181da13a3fb8725517c57f770bd910e77d
Sean Donovan <sdonovan@gatech.edu>
2015-06-04 15:26:56 -0400

    Adding new dig of first million script.

---
+++ dig-million.sh
@@ -0,0 +1,41 @@
+
+python get_dig_from_list.py 1 50000 &
+sleep 5
+python get_dig_from_list.py 50001 50000 &
+sleep 5
+python get_dig_from_list.py 100001 50000 &
+sleep 5
+python get_dig_from_list.py 150001 50000 &
+sleep 5
+python get_dig_from_list.py 200001 50000 &
+sleep 5
+python get_dig_from_list.py 250001 50000 &
+sleep 5
+python get_dig_from_list.py 300001 50000 &
+sleep 5
+python get_dig_from_list.py 350001 50000 &
+sleep 5
+python get_dig_from_list.py 400001 50000 &
+sleep 5
+python get_dig_from_list.py 450001 50000 &
+sleep 5
+python get_dig_from_list.py 500001 50000 &
+sleep 5
+python get_dig_from_list.py 550001 50000 &
+sleep 5
+python get_dig_from_list.py 600001 50000 &
+sleep 5
+python get_dig_from_list.py 650001 50000 &
+sleep 5
+python get_dig_from_list.py 700001 50000 &
+sleep 5
+python get_dig_from_list.py 750001 50000 &
+sleep 5
+python get_dig_from_list.py 800001 50000 &
+sleep 5
+python get_dig_from_list.py 850001 50000 &
+sleep 5
+python get_dig_from_list.py 900001 50000 &
+sleep 5
+python get_dig_from_list.py 950001 50000 &
+
